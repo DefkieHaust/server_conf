0d4eba7ab248ffa3d7add95e5480567a5d938705
DefkieHaust <noreply>
2023-04-22 04:15:16 +0500

    release 2.1

---
+++ backup.py
@@ -0,0 +1,9 @@
+from time import sleep
+from os import system
+from datetime import datetime
+
+while True:
+    system("cp ./db.sqlite3 ./db.sqlite3.bak")
+    system("git pull")
+    print("Backed up database and synced repo at", datetime.now())
+    sleep(10)
