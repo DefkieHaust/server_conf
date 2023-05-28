5bc9ff53cafb921c58031ad3021889890a199dd6
DefkieHaust <noreply>
2023-05-28 13:29:43 -0700

    2.4 fix

--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-zv_%#ypp$0uq#k%gi57a_#103%i=f!m7qw!%ch8&i7$jqkyce("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
